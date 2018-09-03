95e1bc67bd66311e03f4a3d6d2d8a8e953e92d3f
JIANGYUWEI [江雨微] <JIANGYUWEI@kingsoft.com>
2018-09-03 21:41:50 +0800

    5.14

--- Chapter5/src/main/java/com/course/testng/paramter/DataProviderTest.java
+++ Chapter5/src/main/java/com/course/testng/paramter/DataProviderTest.java
@@ -1,21 +1,24 @@
 package com.course.testng.paramter;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class DataProvider {
+public class DataProviderTest {
     //通过DataProvider传递参数自动化
-    @Test(dataProvider = "")
+    @Test(dataProvider = "data")
     public void testDataProvider(String name,int age){
         System.out.println("name =" + name + ";   age = " + age);
     }
 
-    //数据来源
+    //数据来源,通过dataprovider传入上方的方法中
+    @DataProvider(name = "data")
     public Object[][] providerData(){
         Object[][] o = new Object[][]{
                 {"zhangsan",10},
                 {"lisi",20},
                 {"wangwu",30}
         };
+        return o;
     }
 
 }
