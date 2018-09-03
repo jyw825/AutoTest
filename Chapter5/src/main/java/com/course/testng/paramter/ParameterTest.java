9bd729556fe5d719481b4436e6e92a41d6505e45
JIANGYUWEI [江雨微] <JIANGYUWEI@kingsoft.com>
2018-09-03 15:20:43 +0800

    5.13

--- Chapter5/src/main/java/com/course/testng/paramter/ParameterTest.java
+++ Chapter5/src/main/java/com/course/testng/paramter/ParameterTest.java
@@ -1,8 +1,13 @@
 package com.course.testng.paramter;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class ParameterTest {
-    //参数化测试
+    //通过xml文件实现参数化
+    @Test
+    @Parameters({"name","age"})  //接收两个参数
     public void paramTest1(String name,int age){
-        System.out.println("name ="+name);
+        System.out.println("name ="+name+" ; age = "+age);
     }
 }
