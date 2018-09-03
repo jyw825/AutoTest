74b243de6a7e74a73b5777733576f308bd7c000e
JIANGYUWEI [江雨微] <JIANGYUWEI@kingsoft.com>
2018-09-03 14:23:30 +0800

    5.13

---
+++ Chapter5/src/main/java/com/course/testng/paramter/ParameterTest.java
@@ -0,0 +1,8 @@
+package com.course.testng.paramter;
+
+public class ParameterTest {
+    //参数化测试
+    public void paramTest1(String name,int age){
+        System.out.println("name ="+name);
+    }
+}
