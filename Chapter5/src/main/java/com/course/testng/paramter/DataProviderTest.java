d1e44ef8b76c972a7a9eaa2ca6a5366bc19a0d55
JIANGYUWEI [江雨微] <JIANGYUWEI@kingsoft.com>
2018-09-04 12:06:05 +0800

    5.14

--- Chapter5/src/main/java/com/course/testng/paramter/DataProviderTest.java
+++ Chapter5/src/main/java/com/course/testng/paramter/DataProviderTest.java
@@ -3,6 +3,8 @@ package com.course.testng.paramter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 public class DataProviderTest {
     //通过DataProvider传递参数自动化
     @Test(dataProvider = "data")
@@ -20,5 +22,37 @@ public class DataProviderTest {
         };
         return o;
     }
+
+
+
+    //支持根据方法来进行参数传递
+    @Test(dataProvider = "methodData")
+    public void test1(String name,int age){
+        System.out.println("test1111方法  name = "+name+"age = "+age);
+    }
+    @Test(dataProvider = "methodData")
+    public void test2(String name,int age){
+        System.out.println("test222方法  name = "+name+"age = "+age);
+    }
+    @DataProvider(name = "methodData")
+    public Object[][] methodDataTest(Method method){  //method自动会把方法test1和test2传递进来
+        Object[][] result = null;
+        if(method.getName().equals("test1")){
+            result = new Object[][]{
+                    {"zhangsan",20},
+                    {"lisi",25}
+            };
+        }else if(method.getName().equals("test2")){
+            result = new Object[][]{
+                    {"wangwu",50},
+                    {"zhaoliu",60}
+            };
+
+        }
+        return result;
+    }
+
+
+
 
 }
