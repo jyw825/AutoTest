6b30851e735542eda192255f4e41cdcb1bb80599
JIANGYUWEI [江雨微] <JIANGYUWEI@kingsoft.com>
2018-09-06 17:46:50 +0800

    5.17

---
+++ Chapter5/src/main/java/com/course/testng/TimeOutTest.java
@@ -0,0 +1,22 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class TimeOutTest {
+    //超时测试
+    @Test(timeOut = 3000)  //单位为毫秒值
+    public void testSuccess() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println(2);
+    }
+    @Test(timeOut = 2000)
+    public void testFailed() throws InterruptedException{
+        Thread.sleep(3000);
+        System.out.println(3);
+        }
+    @Test
+    public void test1(){
+        System.out.println("1");
+    }
+    }
+
